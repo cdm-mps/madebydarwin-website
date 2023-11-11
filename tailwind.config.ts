5baafd0a9f1cb87de32748c35fdea0e4274b61f8
catarinapm <catarinapdematos@gmail.com>
2023-11-11 22:10:11 +0000

    feat: main content with responsiveness

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,17 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    backgroundImage: {
+      "main-gradient":
+        "linear-gradient(113deg,rgba(25,16,58,1),rgba(40,15,57,0.8),rgba(12,16,41,1))",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
