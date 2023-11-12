f5b16e70de889379599efb6bce65d9c8153fcf14
catarinapm <catarinapdematos@gmail.com>
2023-11-12 01:09:05 +0000

    feat: copy email

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,49 @@
+"use client";
+import clsx from "clsx";
 import Image from "next/image";
+import React, { useEffect } from "react";
 
 export default function Home() {
+  const email = "madebydarwin@gmail.com";
+  const [copied, setCopied] = React.useState<boolean>(false);
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+    }
+  }, [copied]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12 sm:p-24">
-      <p className="text-2xl sm:text-4xl">{"{WIP}"}</p>
-      <div>
-        <p className="text-6xl sm:text-8xl font-bold">MADE BY DARWIN</p>
-        <p className="text-2xl sm:text-4xl">DESIGN AND DEVELOPMENT</p>
+    <main className="flex flex-col items-center p-12 sm:p-24">
+      <div className="flex flex-col items-center justify-between h-[80vh]">
+        <p className="text-2xl sm:text-4xl">{"{WIP}"}</p>
+        <div>
+          <p className="text-6xl sm:text-8xl font-bold">MADE BY DARWIN</p>
+          <p className="text-2xl sm:text-4xl">DESIGN AND DEVELOPMENT</p>
+        </div>
+        <div className="flex flex-col items-center mt-24 gap-4">
+          <p className="text-2xl sm:text-4xl font-bold">REACH US AT</p>
+          <p
+            className="text-xl sm:text-3xl hover:underline cursor-pointer"
+            onClick={() => {
+              setCopied(true);
+              navigator.clipboard.writeText(email);
+            }}
+          >
+            {email}
+          </p>
+        </div>
       </div>
-      <div className="flex flex-col items-center p-24 gap-4">
-        <p className="text-2xl sm:text-4xl font-bold">REACH US AT</p>
-        <p className="text-xl sm:text-3xl hover:underline cursor-pointer">madebydarwin@gmail.com</p>
-      </div>
+      <p
+        className={clsx(
+          "text-base font-bold sm:text-xl mt-4 ",
+          !copied && "hidden"
+        )}
+      >
+        copied!
+      </p>
     </main>
   );
 }
